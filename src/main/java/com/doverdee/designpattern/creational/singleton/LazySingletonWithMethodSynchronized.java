cde35a8b70665f1eccc6c8b2010abc005676959e
DoverDee <doverlee@foxmail.com>
2020-07-26 16:04:33 +0800

    fix LazySingletonWithMethodSynchronized

--- src/main/java/com/doverdee/designpattern/creational/singleton/LazySingletonWithMethodSynchronized.java
+++ src/main/java/com/doverdee/designpattern/creational/singleton/LazySingletonWithMethodSynchronized.java
@@ -1,12 +1,15 @@
 package com.doverdee.designpattern.creational.singleton;
 
 public class LazySingletonWithMethodSynchronized {
-  private LazySingletonWithMethodSynchronized() {
-  }
+    private LazySingletonWithMethodSynchronized() {
+    }
 
-  private static LazySingletonWithMethodSynchronized instance;
+    private static LazySingletonWithMethodSynchronized instance;
 
-  public static synchronized LazySingletonWithMethodSynchronized getInstance() {
-    return instance;
-  }
+    public static synchronized LazySingletonWithMethodSynchronized getInstance() {
+        if (instance == null) {
+            instance = new LazySingletonWithMethodSynchronized();
+        }
+        return instance;
+    }
 }
